075f5acae9cd221de270d46dfdee755fc312e826
Nuriza05 <mnuriza9@gmail.com>
2023-01-04 15:43:09 +0300

    2 commit

--- src/enums/TaxiType.java
+++ src/enums/TaxiType.java
@@ -3,10 +3,30 @@ package enums;
 import java.math.BigDecimal;
 
 public enum TaxiType {
-    STANDART,
-    COMFORT,
-    BUSINESS;
+    STANDART(BigDecimal.valueOf(200),BigDecimal.valueOf(100)),
+    COMFORT(BigDecimal.valueOf(300),BigDecimal.valueOf(200)),
+    BUSINESS(BigDecimal.valueOf(400),BigDecimal.valueOf(300));
     private BigDecimal pricePerKm;
     private BigDecimal priceForLanding;
 
+    TaxiType(BigDecimal pricePerKm, BigDecimal priceForLanding) {
+        this.pricePerKm = pricePerKm;
+        this.priceForLanding = priceForLanding;
+    }
+
+    public BigDecimal getPricePerKm() {
+        return pricePerKm;
+    }
+
+    public void setPricePerKm(BigDecimal pricePerKm) {
+        this.pricePerKm = pricePerKm;
+    }
+
+    public BigDecimal getPriceForLanding() {
+        return priceForLanding;
+    }
+
+    public void setPriceForLanding(BigDecimal priceForLanding) {
+        this.priceForLanding = priceForLanding;
+    }
 }
